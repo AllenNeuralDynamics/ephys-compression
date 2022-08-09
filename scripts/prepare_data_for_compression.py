427e2d3832dc202244796a3c6c4771d14453bcfa
Alessio Buccino <alejoe9187@gmail.com>
2022-08-09 17:53:04 +0200

    Final touches to extended benchmark

--- scripts/prepare_data_for_compression.py
+++ scripts/prepare_data_for_compression.py
@@ -15,11 +15,10 @@ all data are loaded and saved to binary by SI.
 The final datasets are uploaded in the "gs://aind-ephys-compression-benchmark-data" bucket.
 """
 
+from utils import get_oe_stream, gs_download_folder, gs_upload_folder, \
+    s3_download_public_file, s3_download_public_folder
 from pathlib import Path
 
-import boto3
-from botocore.config import Config
-from botocore import UNSIGNED
 import os
 import shutil
 import numpy as np
@@ -34,21 +33,22 @@ import sys
 
 sys.path.append("..")
 
-from utils import get_oe_stream, gs_download_folder, gs_upload_folder, \
-    s3_download_public_file, s3_download_public_folder
 
 ephys_compression_folder_path = Path(__file__).parent.parent
 
 """
 SET YOUR LOCAL DIRECTORY HERE
 """
-ROOT_PATH = Path("/home/alessio/Documents/data/compression")
+#ROOT_PATH = Path("/home/alessio/Documents/data/compression")
+ROOT_PATH = Path("/home/jupyter/data/compression")
 
-job_kwargs = dict(n_jobs=20, chunk_duration="1s", progress_bar=True)
+job_kwargs = dict(n_jobs=12, chunk_duration="5s", progress_bar=True)
 
 compression_bucket_path = "aind-ephys-compression-benchmark-data"
 compression_bucket = f"gs://{compression_bucket_path}"
 delete_tmp_files_as_created = True
+
+print(job_kwargs)
 
 
 fs = gcsfs.GCSFileSystem()
@@ -65,16 +65,16 @@ print("\n\n\nAIND\n\n\n")
 aind_ephys_bucket = "gs://aind-ephys-data/"
 dataset = "aind"
 
-aind_sessions = {"595262_2022-02-21_15-18-07": {"probe": "ProbeA"}, 
+aind_sessions = {"595262_2022-02-21_15-18-07": {"probe": "ProbeA"},
                  "618197_2022-06-21_14-08-06": {"probe": "ProbeC"},
                  "621362_2022-07-14_11-19-36": {"probe": "ProbeA"},
                  "602454_2022-03-22_16-30-03": {"probe": "ProbeB"},
-                 "618382_2022-03-31_14-27-03": {"probe": "ProbeC"}, 
+                 "618382_2022-03-31_14-27-03": {"probe": "ProbeC"},
                  "618318_2022-04-13_14-59-07": {"probe": "ProbeB"},
                  "612962_2022-04-13_19-18-04": {"probe": "ProbeB"},
                  "613373_2022-04-26_15-36-12": {"probe": "ProbeC"},
                  "618384_2022-04-14_15-11-00": {"probe": "ProbeB"},
-                 "612962_2022-04-14_17-17-10": {"probe": "ProbeC"},}
+                 "612962_2022-04-14_17-17-10": {"probe": "ProbeC"}, }
 
 for session, session_data in aind_sessions.items():
     print(session)
@@ -156,11 +156,12 @@ dataset = "ibl"
 
 ibl_sessions = {"CSHZAD026_2020-09-04_probe00": "CSH_ZAD_026/2020-09-04/001/raw_ephys_data/probe00",
                 "CSHZAD029_2020-09-09_probe00": "CSH_ZAD_029/2020-09-09/001/raw_ephys_data/probe00",
-                "SWC054_2020-10-05_probe01": "SWC_054/2020-10-05/001/raw_ephys_data/probe00"}
+                "SWC054_2020-10-05_probe00": "SWC_054/2020-10-05/001/raw_ephys_data/probe00",
+                "SWC054_2020-10-05_probe01": "SWC_054/2020-10-05/001/raw_ephys_data/probe01"}
 
 for session, session_path in ibl_sessions.items():
     print(session)
-    
+
     session_folder = output_folder / dataset / session
     remote_location = f"{compression_bucket_path}/{dataset}/{session}"
 
@@ -176,7 +177,7 @@ for session, session_path in ibl_sessions.items():
             dest = tmp_folder / dataset
 
             s3_download_public_folder(f"{prefix}/{session_path}", dest / session, s3_bucket_ibl, region_name_ibl,
-                                    skip_patterns=skip_patterns)
+                                      skip_patterns=skip_patterns)
 
             cbin_folder = dest / session
 
@@ -209,20 +210,25 @@ prefix = 'visual-coding-neuropixels/raw-data'
 dataset = "mindscope"
 
 mindscope_sessions = {
-    "754312389_756781559": "754312389/756781559/spike_band.dat",
-    "766640955_773592324": "766640955/773592324/spike_band.dat",
-    "797828357_805579745": "797828357/805579745/spike_band.dat",
-    "829720705_832129157": "829720705/832129157/spike_band.dat",}
-#     "766640955/773592324/spike_band.dat",
-#     "797828357/805579745/spike_band.dat",
-#     "829720705/832129157/spike_band.dat",
-# ]
+    "754312389_probe756781559": "754312389/756781559/spike_band.dat",
+    "766640955_probe773592324": "766640955/773592324/spike_band.dat",
+    "797828357_probe805579745": "797828357/805579745/spike_band.dat",
+    "829720705_probe832129157": "829720705/832129157/spike_band.dat",
+    "839557629_probe846401838": "839557629/846401838/spike_band.dat",
+}
 
 binary_dict = dict(num_chan=384, sampling_frequency=30000, dtype="int16", gain_to_uV=0.195, offset_to_uV=0)
 
+# 30 min
+start_second = 1000
+duration = 1800
+
+start_frame = int(start_second * binary_dict["sampling_frequency"])
+end_frame = start_frame + int(duration * binary_dict["sampling_frequency"])
+
 for session, session_path in mindscope_sessions.items():
     print(session)
-    
+
     session_folder = output_folder / dataset / session
     remote_location = f"{compression_bucket_path}/{dataset}/{session}"
 
@@ -237,16 +243,21 @@ for session, session_path in mindscope_sessions.items():
             dest = tmp_folder / dataset
             dat_file_name = session_path.split("/")[-1]
             dest_file = dest / session / dat_file_name
+            print(dest_file)
             if not dest_file.is_file():
-                s3_download_public_file(f"{prefix}/{session_path}", dest, 
+                s3_download_public_file(f"{prefix}/{session_path}", dest_file.parent,
                                         s3_bucket_mindscope, region_name_mindscope)
 
             recording = si.read_binary(dest_file, **binary_dict)
             print(recording)
 
-            # load NP1 probe config probe and save    
+            # load NP1 probe config probe and save
             probegroup = pi.read_probeinterface(ephys_compression_folder_path / "probes" / "NP1_tip_config.json")
             recording = recording.set_probegroup(probegroup)
+
+            # cutout recording
+            recording = recording.frame_slice(start_frame=start_frame, end_frame=end_frame)
+            print(f"Recording cut {recording}")
 
             # save output to binary
             recording.save(folder=session_folder, **job_kwargs)
